1cdf412a1350a1c73da5219b104c25fc6799cdeb
EyesOnly89 <eyesonly89@gmail.com>
2025-06-21 20:06:14 +0200

    Adding Premake File and Ignores for MOROS Divergence.

---
+++ premake5.lua
@@ -0,0 +1,11 @@
+project "ENTT"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.hpp",
+	}
